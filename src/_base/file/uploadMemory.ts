08bf69622dce4cb800f8fcb6dc5abe5db9efc7d8
HuuViet <nguyenvietbn1999@gmail.com>
2021-06-04 18:47:22 +0700

    Forgot save

--- src/_base/file/uploadMemory.ts
+++ src/_base/file/uploadMemory.ts
@@ -4,7 +4,7 @@ const uploadMemory = multer({
   storage: multer.memoryStorage(),
   fileFilter: function (req:any , file: any, cb: any) {
     const mt = file.mimetype;
-    const isOK = mt === 'image/jpg' || mt === 'image/jpeg' || mt === 'image/png';
+    const isOK = mt === 'image/jpg' || mt === 'image/jpeg' || mt === 'image/png' || mt === 'image/gif';
     return cb(null, isOK);
   },
 })
